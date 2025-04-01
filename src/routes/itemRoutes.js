447b9aa5fe69da2e9f7b7eb1a6c076a87db0a07a
elmanee <cp457920@gmail.com>
2025-03-31 19:56:56 -0600

    Update itemRoutes.js

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
 
-router.get('/', itemController.getItems);
+router.get('/api/items', itemController.getItems);
 router.post('/', itemController.createItem);
 
 module.exports = router;
